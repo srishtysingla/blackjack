0a1cae520f1e0e9e3b539bd0c0ef6b4d582949d8
srishtysingla <srishtysingla19@gmail.com>
2025-06-12 21:58:13 +0200

    Allow users to play multiple rounds

--- main.py
+++ main.py
@@ -2,27 +2,33 @@ from game import Game
 
 
 def main():
-    print("Setting up Blackjack!")
-    my_game = Game()
-    my_game.setup()
-    my_game.show_status()
-
-    while my_game.has_ended == False:
-        command = input("Do you want to Hit or Stand? ")
-        command = command.lower()
-        if command == "hit":
-            my_game.player.hit(my_game.deck, my_game.dealer, my_game)
-        elif command == "stand":
-            my_game.player.stand(my_game)
-        else:
-            print("Please enter a valid command")
+    terminate = None
+    while terminate != "y":
+        print("Setting up Blackjack!")
+        my_game = Game()
+        my_game.setup()
         my_game.show_status()
-    if my_game.winner == my_game.player:
-        print("You won!")
-    elif my_game.winner is None:
-        print("It is a push")
-    else:
-        print("You lost!")
+        while my_game.has_ended == False:
+            command = input("Do you want to Hit or Stand? ")
+            command = command.lower()
+            if command == "hit":
+                my_game.player.hit(my_game.deck, my_game.dealer, my_game)
+            elif command == "stand":
+                my_game.player.stand(my_game)
+            else:
+                print("Please enter a valid command")
+            my_game.show_status()
+        if my_game.winner == my_game.player:
+            print("You won!")
+        elif my_game.winner is None:
+            print("It is a push")
+        else:
+            print("You lost!")
+        terminate = input("Do you want to exit? (Y/N) ")
+        terminate = terminate.lower()
+        if terminate == "y":
+            print("Thank you for playing.")
+
 
 
 if __name__ == "__main__":
